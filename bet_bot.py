2052533ef4ba73a8b91566f9d2df2e449f9e58cf
talzaken <talzaken@gmail.com>
2023-12-16 16:24:49 -0500

    Play with formatting

--- bet_bot.py
+++ bet_bot.py
@@ -1,79 +1,9 @@
-"""import os
-from datetime import datetime, timedelta
-
-import discord
-from discord.ext import commands, tasks
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-CHANNEL_ID = os.getenv("CHANNEL_ID")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# Create a connection to SQLite database
-conn = sqlite3.connect('bets.db')
-cursor = conn.cursor()
-
-# Create a table to store bets
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS bets (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        question TEXT,
-        expiration_date TEXT,
-        yes_user TEXT,
-        no_user TEXT,
-        value INTEGER
-    )
-''')
-conn.commit()
-
-# Set up the Discord bot
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-@bot.command(name='place-bet')
-async def place_bet(ctx, question, expiration_date, yes_user, no_user, value):
-    # Store bet in the SQLite database
-    cursor.execute('''
-        INSERT INTO bets (question, expiration_date, yes_user, no_user, value)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (question, expiration_date, yes_user, no_user, value))
-    conn.commit()
-
-    await ctx.send('Bet placed successfully!')
-
-# Task to check for expired bets and send reminders
-@tasks.loop(seconds=60)
-async def check_expired_bets():
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    expired_bets = cursor.execute('SELECT * FROM bets WHERE expiration_date <= ?', (current_time,)).fetchall()
-
-    for bet in expired_bets:
-        bet_id, question, _, _, _, _ = bet
-        channel = bot.get_channel(CHANNEL_ID)  # Replace with your Discord channel ID
-        await channel.send(f'Bet "{question}" has expired!')
-
-# Start the task
-check_expired_bets.start()
-
-# Run the bot
-bot.run(BOT_TOKEN)  # Replace with your Discord bot token
-"""
-
 import os
 import sqlite3
 
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-
 
 
 # Create a connection to SQLite database
@@ -121,7 +51,7 @@ async def place_bet(ctx, question, expiration_date, yes_user, no_user, value):
     await ctx.send('Bet placed successfully!')
 
 
-def format_table(column_names, data):
+def format_table(table_name, column_names, data):
     column_widths = {}
     for col_idx, column in enumerate(column_names):
         column_widths[col_idx] = len(column) + 2
@@ -129,14 +59,18 @@ def format_table(column_names, data):
             column_value_len = len(str(row[col_idx]))
             column_widths[col_idx] = max(column_value_len + 2, column_widths[col_idx])
 
-    table_str = ""
+    divider = (
+            "_" * (sum(column_widths.values()) + len(column_names))
+            + "\n"
+    )
+    table_str = f"{table_name}\n{divider}"
 
     header_str = ""
     for col_idx, col_name in enumerate(column_names):
         whitespace = column_widths[col_idx] - len(col_name)
         header_str += col_name + (" " * whitespace) + "|"
 
-    table_str += header_str + "\n"
+    table_str += header_str + "\n" + divider
 
     for row in data:
         row_str = ""
@@ -144,7 +78,7 @@ def format_table(column_names, data):
             col_value = str(col_value)
             whitespace = column_widths[col_idx] - len(col_value)
             row_str += col_value + (" " * whitespace) + "|"
-        table_str += row_str + "\n"
+        table_str += row_str + "\n" + divider
 
     return f"`{table_str}`"
 
@@ -158,9 +92,9 @@ async def view_bets(ctx):
     bets = bets_query.fetchall()
 
     column_names = ["id", "question", "expiration_date", "yes_user", "no_user", "value"]
-    table_str = format_table(column_names, bets)
+    table_str = format_table("Placed Bets", column_names, bets)
     await ctx.send(table_str)
 
 
-
-bot.run(BOT_TOKEN)
+if __name__ == "__main__":
+    bot.run(BOT_TOKEN)
